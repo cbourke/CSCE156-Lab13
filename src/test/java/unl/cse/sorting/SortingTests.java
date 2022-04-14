c0c8e02dd99d5eb261cf11c7002ccea5b2042280
Chris Bourke <cbourke@cse.unl.edu>
2022-04-14 09:28:28 -0500

    added a timeout annotation (5 seconds) to kill infinite loops

--- src/test/java/unl/cse/sorting/SortingTests.java
+++ src/test/java/unl/cse/sorting/SortingTests.java
@@ -1,6 +1,7 @@
 package unl.cse.sorting;
 
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Timeout;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -12,8 +13,12 @@ public class SortingTests {
 	/**
 	 * Repeatedly (10 times) tests {@link SortingAlgorithms#insertionSort(List)} on
 	 * a small (100) randomly selected list of {@link Location} instances.
+	 * 
+	 * Each test will time out after a maximum of 5 seconds.  If code takes longer
+	 * than 5 seconds it is likely stuck in an infinite loop.
 	 */
 	@RepeatedTest(10)
+	@Timeout(5)
 	public void insertionSortTest() {
 		List<Location> locations = LocationUtils.getRandomList(100);
 		List<Location> actual = new ArrayList<>(locations);
@@ -27,8 +32,12 @@ public class SortingTests {
 	/**
 	 * Repeatedly (10 times) tests {@link SortingAlgorithms#quickSort(List)} on a
 	 * small (100) randomly selected list of {@link Location} instances.
+	 * 
+	 * Each test will time out after a maximum of 5 seconds.  If code takes longer
+	 * than 5 seconds it is likely stuck in an infinite loop.
 	 */
 	@RepeatedTest(10)
+	@Timeout(5)
 	public void quickSortTest() {
 		List<Location> locations = LocationUtils.getRandomList(100);
 		List<Location> actual = new ArrayList<>(locations);
